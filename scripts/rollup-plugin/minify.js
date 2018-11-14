cb8ff6313e7e49d8ed085dfc082db3becc7a9113
Toru Nagashima <star.ctor@gmail.com>
2018-11-15 05:04:26 +0900

    ⬆️ update dependencies

--- scripts/rollup-plugin/minify.js
+++ scripts/rollup-plugin/minify.js
@@ -2,12 +2,12 @@
  * @author Toru Nagashima <https://github.com/mysticatea>
  * See LICENSE file in root directory for full license.
  */
-import uglify from "uglify-es"
+import terser from "terser"
 
 export default () => ({
     name: "minify",
 
     transformBundle(source) {
-        return uglify.minify(source).code
+        return terser.minify(source).code
     },
 })
