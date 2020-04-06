22f80855abbac78f22f96e7be9e8b3c0566e643b
Toru Nagashima <public@mysticatea.dev>
2020-04-06 18:47:07 +0900

    🐛 fix css link

--- scripts/rollup-plugin/css.js
+++ scripts/rollup-plugin/css.js
@@ -5,20 +5,18 @@
 import path from "path"
 import fs from "fs-extra"
 
+const URL_PATTERN = /url\((["']?)(.+?)\1\)/gu
+
 export default () => {
     const contents = new Map()
     const files = new Map()
 
-    async function extractFiles(importerId, cssText) {
-        const pattern = /url\((["']?)(.+?)\1\)/gu
-        const promises = []
-
-        let m = null
-        while ((m = pattern.exec(cssText)) != null) {
-            const href = m[2]
+    function extractFiles(cssText, importerId, outLoadingPromises) {
+        return cssText.replace(URL_PATTERN, (code, _quote, href) => {
             if (/^(?:data|https?):/u.test(href)) {
-                continue
+                return code
             }
+
             const filePath = path.resolve(path.dirname(importerId), href)
             const filename = path.basename(filePath)
             const promise = fs.readFile(filePath).then(content => {
@@ -40,10 +38,10 @@ export default () => {
                 // Store it
                 files.set(filename, { content, importerId })
             })
-            promises.push(promise)
-        }
+            outLoadingPromises.push(promise)
 
-        await Promise.all(promises)
+            return `url(${JSON.stringify(filename)})`
+        })
     }
 
     return {
@@ -54,15 +52,32 @@ export default () => {
                 return null
             }
 
-            const content = await fs.readFile(id, "utf8")
-            contents.set(id, content)
-            await extractFiles(id, content)
+            // Load this CSS file
+            const rawContent = await fs.readFile(id, "utf8")
 
+            // Resolve files of `url(...)`
+            const loadingPromises = []
+            const content = extractFiles(
+                rawContent,
+                id,
+                /* out */ loadingPromises,
+            )
+            // Wait for loading of the files.
+            await Promise.all(loadingPromises)
+
+            // Store it
+            contents.set(id, content)
+
+            // Make it empty.
+            // The CSS content will be outputed as single `monaco.css` file.
             return ""
         },
 
-        renderChunk(source) {
-            return `import "./monaco.css";\n${source}`
+        renderChunk(code, chunk) {
+            if (chunk.name !== "monaco") {
+                return null
+            }
+            return `import "./monaco.css";\n${code}`
         },
 
         async writeBundle() {
