3d35912a820809145cf9284e750b9d5a56c4a739
Toru Nagashima <public@mysticatea.dev>
2020-04-06 18:13:42 +0900

    ✨ upgrade dependencies

--- scripts/rollup-plugin/css.js
+++ scripts/rollup-plugin/css.js
@@ -7,6 +7,44 @@ import fs from "fs-extra"
 
 export default () => {
     const contents = new Map()
+    const files = new Map()
+
+    async function extractFiles(importerId, cssText) {
+        const pattern = /url\((["']?)(.+?)\1\)/gu
+        const promises = []
+
+        let m = null
+        while ((m = pattern.exec(cssText)) != null) {
+            const href = m[2]
+            if (/^(?:data|https?):/u.test(href)) {
+                continue
+            }
+            const filePath = path.resolve(path.dirname(importerId), href)
+            const filename = path.basename(filePath)
+            const promise = fs.readFile(filePath).then(content => {
+                // Check conflict
+                const file = files.get(filename)
+                if (file) {
+                    if (content.equals(file.content)) {
+                        return
+                    }
+                    const cwd = process.cwd()
+                    const importers = [
+                        path.relative(cwd, importerId),
+                        path.relative(cwd, file.importerId),
+                    ].join('" and "')
+                    throw new Error(
+                        `Naming conflicted: "${filename}" (from "${importers}")`,
+                    )
+                }
+                // Store it
+                files.set(filename, { content, importerId })
+            })
+            promises.push(promise)
+        }
+
+        await Promise.all(promises)
+    }
 
     return {
         name: "css",
@@ -16,7 +54,9 @@ export default () => {
                 return null
             }
 
-            contents.set(id, await fs.readFile(id, "utf8"))
+            const content = await fs.readFile(id, "utf8")
+            contents.set(id, content)
+            await extractFiles(id, content)
 
             return ""
         },
@@ -31,6 +71,9 @@ export default () => {
                 "dist/monaco.css",
                 Array.from(contents.values()).join("\n\n"),
             )
+            for (const [filename, { content }] of files) {
+                await fs.writeFile(`dist/${filename}`, content)
+            }
         },
     }
 }
